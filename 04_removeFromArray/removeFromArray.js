2ff755397416b59e7e69cf4a685ef7e67da48f49
rektdie <rektdie@gmail.com>
2023-05-21 03:09:26 +0200

    Extending the previous commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (const target of args) {
+        if (array.includes(target)) {
+            array.splice(array.indexOf(target), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    let sum = 0;
 
+    if (from < 0 || to < 0 || Number.isInteger(from) == false || 
+    Number.isInteger(to) == false) return "ERROR";
+
+    const temp = from;
+    if (from > to) {
+        from = to;
+        to = temp;
+    }
+
+    for (let i = from; i <= to; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
